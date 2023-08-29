4e2ec3dd421545f0e07e3f0991c2f4d9007fedfe
chenhui <gaischen@163.com>
2023-08-29 10:03:46 +0800

    reply app page

---
+++ lib/compose_page.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class ComposePage extends StatelessWidget {
+  const ComposePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
+import 'package:gallery/compose_page.dart';
+import 'package:gallery/model/email_store.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ReplyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ReplyApp extends StatefulWidget {
+  const ReplyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  static Route createComposeRoute(RouteSettings settings) {
+    return PageRouteBuilder<void>(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            const ComposePage(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeThroughTransition(
+            fillColor: Theme.of(context).cardColor,
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          );
+        },
+        settings: settings);
   }
 
   @override
+  State<StatefulWidget> createState() => _ReplyAppStete();
+}
+
+class _ReplyAppStete extends State<ReplyApp> with RestorationMixin {
+  final _RestorableEmailState _appState = _RestorableEmailState();
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  String? get restorationId => 'repluState';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_appState, 'state');
+  }
+}
+
+class _RestorableEmailState extends RestorableListenable<EmailStore> {
+  @override
+  EmailStore createDefaultValue() {
+    return EmailStore();
+  }
+
+  @override
+  EmailStore fromPrimitives(Object? data) {
+    final appState = EmailStore();
+
+    return appState;
+  }
+
+  @override
+  Object? toPrimitives() {
+    return <String, dynamic>{
+      'selectedEmailId': value.selectedEmailId,
+      // The index of the MailboxPageType enum is stored, since the value
+      // has to be serializable.
+      'selectedMailboxPage': value.selectedMailboxPage.index,
+      'onSearchPage': value.onSearchPage,
+      'starredEmailIds': value.starredEmailIds.toList(),
+      'trashEmailIds': value.trashEmailIds.toList(),
+    };
   }
 }
